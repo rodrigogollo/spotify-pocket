a0cbb7754656b8f8812af3287ac4a17d63dca5b0
rodrigollo <rodrigogollo.nh@gmail.com>
2024-10-07 22:10:38 -0300

    add spotify access_token and get_artist

---
+++ src-tauri/src/spotify.rs
@@ -0,0 +1,69 @@
+use reqwest::blocking::Client;
+use serde_json::Value;
+use dotenvy::dotenv;
+use std::env;
+use tauri::command;
+
+
+#[tauri::command]
+pub fn request_test() -> String{
+  dotenv().ok();
+  let client_id = env::var("SPOTIFY_CLIENT_ID").expect("SPOTIFY_CLIENT_ID not found in .env");
+  let client_secret = env::var("SPOTIFY_CLIENT_SECRET").expect("SPOTIFY_CLIENT_SECRET not found in .env");
+
+  let token = access_token(&client_id, &client_secret);
+  // println!("{}", token);
+  let artist= get_artist(token);
+  println!("{}", artist);
+  artist
+}
+
+fn get_artist(token: String) -> String {
+  let http_client = Client::new();
+
+  let response = http_client.get("https://api.spotify.com/v1/artists/4Z8W4fKeB5YxbusRsdQVPb")
+    .header("Authorization", format!("Bearer {}", token))
+    .send()
+    .expect("failed to get response")
+    .text()
+    .expect("failed to get payload");
+
+  let json: Value = serde_json::from_str(&response).expect("Failed to parse JSON");
+
+  let pretty_json = serde_json::to_string_pretty(&response)
+    .expect("Failed to format JSON");
+
+  println!("{}", pretty_json);
+
+  let artist_name = json["name"]
+    .as_str()
+    .expect("No access token found")
+    .to_string();
+  
+   artist_name
+}
+
+fn access_token(client_id: &str, client_secret: &str) -> String {
+  let http_client = Client::new();    
+  let params = [
+    ("grant_type", "client_credentials"),
+    ("client_id", client_id),
+    ("client_secret", client_secret),
+  ];
+  let url = "https://accounts.spotify.com/api/token";
+
+  let response = http_client.post(url) 
+  .form(&params)
+  .send()
+  .expect("failed to get response")
+  .text()
+  .expect("failed to get payload");
+
+  let json: Value = serde_json::from_str(&response).expect("Failed to parse JSON");
+  let access_token = json["access_token"]
+    .as_str()
+    .expect("No access token found")
+    .to_string();
+
+  access_token
+}
--- src/App.tsx
+++ src/App.tsx
@@ -5,29 +5,16 @@ import "./App.css";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [artist, setArtist] = useState("");
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
+    setGreetMsg(await invoke("request_test", { artist }));
   }
 
   return (
     <div className="container">
       <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
       <p>Teste Rodrigo</p>
 
       <form
@@ -39,7 +26,7 @@ function App() {
       >
         <input
           id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
+          onChange={(e) => setArtist(e.currentTarget.value)}
           placeholder="Enter a name..."
         />
         <button type="submit">Greet</button>

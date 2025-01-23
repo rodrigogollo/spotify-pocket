773fdfa2d60f9c2f3f840beb235eb876f400141d
Rodrigo Gollo <rodrigogollo.nh@gmail.com>
2025-01-22 21:28:22 -0300

    fixing tags

--- src/components/Tags/Tags.tsx
+++ src/components/Tags/Tags.tsx
@@ -12,13 +12,13 @@ const Tags = () => {
   return (
     <div className="tags-container">
       <span id="top" className={filter == "top" ? "active" : ""} onClick={handleFilterClick}>Top Results</span>
-      <span id="artist" className={filter == "artist" ? "active" : ""} onClick={handleFilterClick}>artist</span>
-      <span id="track" className={filter == "track" ? "active" : ""} onClick={handleFilterClick}>track</span>
-      <span id="album" className={filter == "album" ? "active" : ""} onClick={handleFilterClick}>album</span>
-      <span id="playlist" className={filter == "playlist" ? "active" : ""} onClick={handleFilterClick}>playlist</span>
-      <span id="show" className={filter == "show" ? "active" : ""} onClick={handleFilterClick}>show</span>
-      {/* <span id="episode" className={filter == "episode" ? "active" : ""} onClick={handleFilterClick}>episode</span> */}
-      {/* <span id="audiobook" className={filter == "audiobook" ? "active" : ""} onClick={handleFilterClick}>audiobook</span> */}
+      <span id="artist" className={filter == "artist" ? "active" : ""} onClick={handleFilterClick}>Artist</span>
+      <span id="track" className={filter == "track" ? "active" : ""} onClick={handleFilterClick}>Songs</span>
+      <span id="album" className={filter == "album" ? "active" : ""} onClick={handleFilterClick}>Albums</span>
+      <span id="playlist" className={filter == "playlist" ? "active" : ""} onClick={handleFilterClick}>Playlists</span>
+      <span id="show" className={filter == "show" ? "active" : ""} onClick={handleFilterClick}>Shows</span>
+      <span id="episode" className={filter == "episode" ? "active" : ""} onClick={handleFilterClick}>episode</span>
+      <span id="audiobook" className={filter == "audiobook" ? "active" : ""} onClick={handleFilterClick}>audiobook</span>
     </div>
   )
 }
